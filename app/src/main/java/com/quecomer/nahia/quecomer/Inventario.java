0cacae049035f9808becbc2b84c71756df4f6836
Naugh <j.rodriguezbarr@alumnos.urjc.es>
2016-12-11 13:18:48 +0100

    Lista strings implementada

--- app/src/main/java/com/quecomer/nahia/quecomer/Inventario.java
+++ app/src/main/java/com/quecomer/nahia/quecomer/Inventario.java
@@ -1,29 +1,80 @@
 package com.quecomer.nahia.quecomer;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class Inventario extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
+    ListView lista;
+    private String m_Text = "";
+    private FloatingActionButton fab;
+    final Context c = this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventario);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        lista = (ListView) findViewById(R.id.lista);
+
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        //Creando prompt
+
+        lista.setAdapter(adapter);
+        //Botón fab
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                       //.setAction("Action", null).show();
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+                View mView = layoutInflaterAndroid.inflate(R.layout.input_dialog, null);
+                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+                alertDialogBuilderUserInput.setView(mView);
+
+                final EditText userInputDialogEditText = (EditText) mView.findViewById(R.id.userInputDialog);
+                lista.setAdapter(adapter);
+                alertDialogBuilderUserInput
+                        .setCancelable(false)
+                        .setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                // ToDo get user input here
+                                adapter.add(userInputDialogEditText.getText().toString());
+                                lista.setAdapter(adapter);
+                            }
+                        })
+
+                        .setNegativeButton("Cancelar",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogBox, int id) {
+                                        dialogBox.cancel();
+                                    }
+                                });
+
+                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+                alertDialogAndroid.show();
             }
         });
+
+
     }
 
 }
